796c504a02d6b9dca43f69de6bfd62660349ab13
ViniciusCardoso19 <vinisilvacardoso10@gmail.com>
2025-09-02 19:18:13 -0300

    Add files via upload

---
+++ algoritmo.js
@@ -0,0 +1,11 @@
+// Fazendo café
+primeiroPasso = "1. Coloque água na chaleira \n"
+segundoPasso = "2. Aqueça até ferver \n"
+terceiroPasso = "3. Coloque café no filtro \n"
+quartoPasso = "4. Despeje água quente no café \n"
+quintoPasso = "5. Despeje leite \n"
+sextoPasso = "6. Acrescente acucar \n"
+setimoPasso = "7. Sirva \n"
+
+// utilizando o console.log para exibir as informações
+console.log(primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso,sextoPasso, setimoPasso)
---
+++ bolo.js
@@ -0,0 +1,26 @@
+//Fazendo bolo 
+primeiroPasso = "No liquidificador, bata a água, o Nesquick, a manteiga, os ovos e o acucar até ficar homogêneo \n"
+segundoPasso = "Transfira a mistura para uma tigela \n"
+terceiroPasso = "Adicione a farinha e o fermento \n"
+quartoPasso = "Misture com uma colher \n"
+sextoPasso = "Despeje a mistura em uma forma de buraco \n"
+quintoPasso = "Enfarinhe a forma de buraco \n"
+setimoPasso = "Leve ao forno medio preaquecido por 35 minutos \n"
+oitavoPasso = "Retire e deixe esfriar \n"
+nonoPasso = "Ao esfriar, desinforme e corte a massa na horizontal em 3 pedaços iguais \n"
+decimoPasso = "Reserve a massa \n"
+decimo1Passo = "Para a cobertura, pegue uma panela \n"
+decimo2Passo = "Na panela, misture o morango, o acucar e o suco de limão \n"
+decimo3Passo = "Leve a mistura ao fogo baixo e deixe levantar fervura \n"
+decimo4Passo = "Cozinhe por 5 minutos e deixe esfriar \n"
+decimo5Passo = "Acrescente 1/3 da cobertura na superfície do primeiro pedaco da massa \n"
+decimo6Passo = "Coloque o segundo pedaco da massa em cima da cobertura do primeiro pedaco de massa \n"
+decimo7Passo = "Acrescente metade do restante da cobertura na superfície do segundo pedaco de massa \n"
+decimo8Passo = "Coloque o terceiro pedaco da massa em cima da cobertura do segundo pedaco de massa \n"
+decimo9Passo = "Acrescente o restante da cobertura sobre o bolo \n"
+vigesimoPasso = "Acrescente morangos frescos sobre a cobertura do bolo \n"
+vigesimo1Passo = "Despeje leite condensado em formas horizontais na superfície do bolo \n"
+vigesimo2Passo = "Sirva \n"
+
+// utilizando o console.log para exibir informações
+console.log(primeiroPasso, segundoPasso, terceiroPasso, quartoPasso, quintoPasso, sextoPasso, setimoPasso, oitavoPasso, nonoPasso, decimoPasso, decimo1Passo, decimo2Passo,decimo3Passo, decimo4Passo, decimo5Passo, decimo6Passo, decimo7Passo, decimo8Passo, decimo9Passo, vigesimoPasso, vigesimo1Passo, vigesimo2Passo)
